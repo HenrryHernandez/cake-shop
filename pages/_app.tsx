d203f7c93fdd719c54af067c15eeccd18ccdd6e3
HenrryHernandez <jenrruhndz@gmail.com>
2024-01-20 19:53:44 -0600

    delete stocks page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import "../styles/globals.css";
 import "../styles/Homepage.css";
 import "../styles/components/ui/Footer.css";
 import "../styles/components/ui/Navbar.css";
-import "../styles/pages/Stock.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
